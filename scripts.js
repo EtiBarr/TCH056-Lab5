7a86d12ef48d3d39e1a710e41226d80404a0dd8f
7anane-b <144293971+7anane-b@users.noreply.github.com>
2024-01-18 15:15:27 -0500

    ?????

--- scripts.js
+++ scripts.js
@@ -2,14 +2,20 @@ function genererGrille(nbLignes, nbColonnes){
     let tblContainer = document.getElementById("grille-jeu");
     let tableau = document.createElement("table");
     let i, j;
+    let grille = [];
     for(i= 0; i<nbLignes; i++){
         let ligne = document.createElement("tr");
+        grille[i]=[];
         for(j=0; j<nbColonnes; j++){
             let colonne = document.createElement("td");
             colonne.innerHTML = 0;
             ligne.appendChild(colonne);
+            grille[i][j] = 0;
         }
         tblContainer.appendChild(ligne);
     }
-    tblContainer.appendChild(tableeau);
+    tblContainer.appendChild(tableau);
+    console.log(grille);
 }
+
+
